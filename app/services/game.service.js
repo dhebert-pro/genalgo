35726265984cc919eefa467d5d12bc9a4d931e0b
David Hébert <pro.david.hebert@gmail.com>
2020-10-01 00:46:11 +0200

    Create game rules

--- app/services/game.service.js
+++ app/services/game.service.js
@@ -9,14 +9,68 @@ const hasAWinner = agents => {
     return getScore(agents[0]) > getScore(agents[1]);
 };
 
-const launchGame = (agent1, agent2) => {
-    if (randomInt(1) === 0) {
-        agent1.winning = agent1.winning + 1;
-        agent2.losing = agent2.losing + 1;
+const getInformationsFromBoard = board => ([
+    board.nbSticks
+]);
+
+const getBoardFromInformations = informations => ({
+    "nbSticks": informations[0]
+})
+
+const removeSticks = nbSticks => board => {
+    if (board.nbSticks - nbSticks > 0) {
+        return {
+            ...board,
+            "nbSticks": board.nbSticks - nbSticks
+        };
     } else {
-        agent1.losing = agent1.losing + 1;
-        agent2.winning = agent2.winning + 1;
+        return false;
     }
+}
+
+const getResultFromInformation = (neuralNetwork, entryValues) => {
+    
+};
+
+const launchGame = (agent1, agent2) => {
+    const agents = [agent1, agent2];
+    let board = {
+        "nbSticks": 20
+    };
+    let firstPlayer = randomInt(1);
+    let playingAgent = agents[firstPlayer];
+    let otherAgent = agents[1 - firstPlayer];
+
+    const possibleMoves = [removeSticks(1), removeSticks(2), removeSticks(3)];
+
+    let bestScore = Number.NEGATIVE_INFINITY;
+    let bestBoard;
+
+    while (board.nbSticks > 0) {
+
+        const possibleInformations = possibleMoves.filter.map(move => {
+            getInformationsFromBoard(move(board));
+        });
+
+        possibleInformations.forEach(possibleInformation => {
+            const result = getResultFromInformation(playingAgent, possibleInformation);
+            if (result > bestScore) {
+                bestScore = result;
+                bestBoard = getBoardFromInformations(possibleInformation);
+            }
+        });
+        
+        board = bestBoard;
+
+        firstPlayer = 1 - firstPlayer;
+        playingAgent = agents[firstPlayer];
+        otherAgent = agents[1 - firstPlayer];
+
+    }
+    
+    otherAgent.winning = otherAgent.winning + 1;
+    playingAgent.losing = playingAgent.losing + 1;
+
 };
 
 const launchGames = agents => {
