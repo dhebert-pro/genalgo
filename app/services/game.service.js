0339b7a0699887f9fd6d57b364539c9fe7b04115
David Hébert <pro.david.hebert@gmail.com>
2020-10-08 12:30:49 +0200

    Add moves in database

--- app/services/game.service.js
+++ app/services/game.service.js
@@ -1,8 +1,26 @@
 const Generation = require('../models/generation.model');
 const AgentService = require('./agent.service');
+const MoveService = require('./move.service');
 const GenerationService = require('./generation.service');
 const randomInt = require('random-int');
 const { v4: uuidv4 } = require('uuid');
+
+const REMOVE_STICKS = 'REMOVE_STICKS';
+
+const removeSticks = nbSticks => board => {
+    if (board.nbSticks - nbSticks >= 0) {
+        return {
+            ...board,
+            'nbSticks': board.nbSticks - nbSticks
+        };
+    } else {
+        return false;
+    }
+}
+
+const gameFunctions = {
+    REMOVE_STICKS: removeSticks
+}
 
 const getScore = agent => agent.winning * 3;
 
@@ -14,17 +32,6 @@ const getBoardFromInformations = informations => {
     if (informations && informations[0] !== false) {
         return {
             'nbSticks': informations[0]
-        };
-    } else {
-        return false;
-    }
-}
-
-const removeSticks = nbSticks => board => {
-    if (board.nbSticks - nbSticks >= 0) {
-        return {
-            ...board,
-            'nbSticks': board.nbSticks - nbSticks
         };
     } else {
         return false;
@@ -65,66 +72,76 @@ const getResultFromInformation = (neuralNetwork, entryValues) => {
     return currentValues;
 };
 
-const getPossibleMoves = board => {
-    let possibleMoves = [removeSticks(1)];
-    if (board.nbSticks > 1) {
-        possibleMoves = [...possibleMoves, removeSticks(2)];
-    };
-    if (board.nbSticks > 2) {
-        possibleMoves = [...possibleMoves, removeSticks(3)];
-    };
-    return possibleMoves;
+const getPossibleMoves = (moves, board) => {
+    return moves.filter(move => {
+        const nbSticks = move.params[0];
+        return (move.name === REMOVE_STICKS && board.nbSticks >= nbSticks);
+    }).map(move => {
+        return gameFunctions[move.name](move.params[0]);
+    });
 }
 
-const launchGame = (agent1, agent2) => {
+const playMove = (moves, agents, board, activePlayer) => {
+    const playingAgent = agents[activePlayer];
+    const possibleMoves = getPossibleMoves(moves, board);
+
+    if (possibleMoves.length === 0) {
+        throw new Error('Aucun coup n\'est possible');
+    }
+
+    let bestScore = Number.NEGATIVE_INFINITY;
+    let bestBoard;
+
+    const possibleInformations = possibleMoves.map(move => {
+        return getInformationsFromBoard(move(board));
+    });
+
+    possibleInformations.filter(x => !!x).forEach(possibleInformation => {
+        const result = getResultFromInformation(playingAgent, possibleInformation)[0];
+        if (result > bestScore) {
+            bestScore = result;
+            bestBoard = getBoardFromInformations(possibleInformation);
+        }
+    });
+
+    if (bestBoard.nbSticks === 0) {
+        return activePlayer;
+    } else {
+        return playMove(moves, agents, bestBoard, 1 - activePlayer);
+    }
+};
+
+const launchGame = (moves, agent1, agent2) => {
     const agents = [agent1, agent2];
-    let board = {
+    const board = {
         'nbSticks': 20
     };
-    let firstPlayer = randomInt(1);
-    let playingAgent = agents[firstPlayer];
-    let otherAgent = agents[1 - firstPlayer];
+    const firstPlayer = randomInt(1);
 
-    while (board.nbSticks > 0) {
-        let possibleMoves = getPossibleMoves(board);
-
-        let bestScore = Number.NEGATIVE_INFINITY;
-        let bestBoard;
-
-        const possibleInformations = possibleMoves.map(move => {
-            return getInformationsFromBoard(move(board));
-        });
-
-        possibleInformations.filter(x => !!x).forEach(possibleInformation => {
-            const result = getResultFromInformation(playingAgent, possibleInformation)[0];
-            if (result > bestScore) {
-                bestScore = result;
-                bestBoard = getBoardFromInformations(possibleInformation);
-            }
-        });
-
-        board = bestBoard;
-
-        firstPlayer = 1 - firstPlayer;
-        playingAgent = agents[firstPlayer];
-        otherAgent = agents[1 - firstPlayer];
-
-    }
+    const activePlayer = playMove(moves, agents, board, firstPlayer);
     
-    playingAgent.winning = playingAgent.winning + 1;
-    otherAgent.losing = otherAgent.losing + 1;
+    const playingAgent = agents[activePlayer];
+    const otherAgent = agents[1 - activePlayer];
+    playingAgent.losing = playingAgent.losing + 1;
+    otherAgent.winning = otherAgent.winning + 1;
 
 };
 
-const launchGames = agents => {
+const launchGames = (moves, agents) => {
     randomAgents = agents.sort(() => Math.random() - 0.5);
     for (let indexAgent = 0; indexAgent < (randomAgents.length - 1) / 2; indexAgent++) {
         const agent1 = randomAgents[2 * indexAgent];
         const agent2 = randomAgents[2 * indexAgent + 1];
-        launchGame(agent1, agent2);
+        launchGame(moves, agent1, agent2);
     }
-    return randomAgents;
 };
+
+const launchGameIteration = (moves, agents, iteration) => {
+    launchGames(moves, agents);
+    if (iteration < 100) {
+        return launchGameIteration(moves, agents, iteration + 1);
+    }
+}
 
 const launch = agents => {
     let playingAgents = agents.map(agent => ({
@@ -133,24 +150,21 @@ const launch = agents => {
             'losing': 0
     }));
 
-    let nbIterations = 0;
+    return MoveService.find().then(moves => {
+        launchGameIteration(moves, playingAgents, 1);
+        
+        playingAgents.forEach(agent => { AgentService.create(agent); });
 
-    while (nbIterations < 100) {
-        playingAgents = launchGames(playingAgents);
-        nbIterations++;
-    }
+        playingAgents = playingAgents.sort((agent1, agent2) => {
+            return getScore(agent2) - getScore(agent1);
+        });
 
-    playingAgents.forEach(agent => { AgentService.create(agent); });
-
-    playingAgents = playingAgents.sort((agent1, agent2) => {
-        return getScore(agent2) - getScore(agent1);
+        const winner = playingAgents[0];
+        return {
+            'winning': winner.winning,
+            'losing': winner.losing
+        };
     });
-
-    const winner = playingAgents[0];
-    return {
-        'winning': winner.winning,
-        'losing': winner.losing
-    };
 };
 
 const createRandomAgents = () => {
@@ -182,12 +196,13 @@ exports.generate = () => {
         count => {
             if (count === 0) {
                 const agents = createRandomAgents();
-                const winner = launch(agents);
-                return GenerationService.create({
-                    generation: 1,
-                    'winner': winner.winner
-                }).then(() => {
-                    return { count: 1 };
+                return launch(agents).then(winner => {
+                    return GenerationService.create({
+                        generation: 1,
+                        'winner': winner.winner
+                    }).then(() => {
+                        return { count: 1 };
+                    });
                 });
             } else {
                 return { count: 0 }
